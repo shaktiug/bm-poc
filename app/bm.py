c83663053668be5f7469969e0011552d2e5189e3
shaksing <shakti_singh@mckinsey.com>
2024-08-02 14:06:56 +0530

    added connection details

--- app/bm.py
+++ app/bm.py
@@ -7,3 +7,15 @@ app = Flask(__name__)
 # Read MySQL configuration from config.ini
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+db_config = {
+    'user': config['mysql']['user'],
+    'password': config['mysql']['password'],
+    'host': config['mysql']['host'],
+    'database': config['mysql']['database']
+}
+
+# Function to get a database connection
+def get_db_connection():
+    conn = mysql.connector.connect(**db_config)
+    return conn
