ac022f8fa1993ad60b3042edc467ec72e88f69bb
shaksing <shakti_singh@mckinsey.com>
2024-08-04 11:21:58 +0530

    updated aks terraform

--- modules/bm-infra/aks.tf
+++ modules/bm-infra/aks.tf
@@ -37,7 +37,7 @@ resource "azurerm_kubernetes_cluster" "my_aks" {
 resource "azurerm_role_assignment" "role_acrpull" {
   scope                            = azurerm_container_registry.acr.id
   role_definition_name             = "AcrPull"
-  principal_id                     = var.infra_vars.aks_service_principal.client_id
+  principal_id                     = data.azuread_service_principal.aks_sp.object_id
   skip_service_principal_aad_check = true
 }
 
